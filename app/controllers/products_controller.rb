ac5096474658d242fd0d7458a563dc4e458c9f22
Derek Dannevig <ddannevig@derek-dannevig.local>
2024-10-25 00:18:07 -0300

    feat: products controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class ProductsController < ApplicationController
+  def show
+    render json: Product.find_by(identifier: params[:identifier]), status: :ok
+  end
+
+  def index
+    render json: Product.all
+  end
+
+  def create
+    render json: Product.create!(create_product_params)
+  end
+
+  def update
+    product = Product.find_by!(identifier: params[:identifier])
+    product.update!(name: params.require(:name))
+    render json: product
+  end
+
+  def destroy
+    Product.find_by!(identifier: params[:identifier]).destroy
+    render :ok
+  end
+
+  private
+
+  def create_product_params
+    params.require(%i[name identifier])
+    params.permit(%i[name identifier])
+  end
+end
