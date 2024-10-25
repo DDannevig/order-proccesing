499c1ac3936edd1e537ee15f828a4bca6507a5ad
Derek Dannevig <ddannevig@derek-dannevig.local>
2024-10-25 19:34:54 -0300

    feat: switch to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2024_10_25_032817) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "deposits", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
