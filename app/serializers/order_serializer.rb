a7277f88f5fb5a84d695239ada44b7f427963031
Derek Dannevig <ddannevig@derek-dannevig.local>
2024-10-27 01:13:56 -0300

    feat: order serializer

---
+++ app/serializers/order_serializer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class OrderSerializer < ActiveModel::Serializer
+  attributes :identifier, :deposit, :products
+
+  def deposit
+    object.deposit.name
+  end
+
+  def products
+    object.order_products.map do |order_product|
+      { name: order_product.product.name,
+        ordered_quantity: order_product.quantity,
+        stock_quantity: order_product.product.stocks.find_by(deposit: object.deposit).quantity }
+    end
+  end
+end
