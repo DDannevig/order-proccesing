55659b8b7de0b01cd74d3c4a4585d172e91bcb0c
Derek Dannevig <ddannevig@derek-dannevig.local>
2024-11-23 15:43:25 -0300

    feat: optimized query

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    render json: Order.all
+    render json: detailed_orders_information
   end
 
   def create
@@ -26,6 +26,20 @@ class OrdersController < ApplicationController
   def order_products_attributes
     params[:products].map do |product|
       { product: Product.find_by!(identifier: product[:identifier]), quantity: product[:quantity] }
+    end
+  end
+
+  def detailed_orders_information
+    query = "SELECT o.identifier, d.name AS deposit, o.status, o.order_prepared_at,
+    (json_agg(json_build_object('quantity', op.quantity, 'name', p.name, 'stock_quantity',
+    (SELECT s.quantity FROM stocks s
+    WHERE (s.deposit_id = d.id AND s.product_id = p.id))))) as products FROM orders o
+    JOIN deposits d ON o.deposit_id = d.id JOIN order_products op ON op.order_id = o.id
+    JOIN products p ON p.id = op.product_id
+    GROUP BY o.identifier, deposit, o.status, o.order_prepared_at ORDER BY deposit, o.identifier"
+
+    ActiveRecord::Base.connection.execute(query).to_a.map do |order|
+      order.merge(products: JSON.parse(order['products']))
     end
   end
 end
